6eb5959ee1e30bb3df1a04d5772a16777058de2d
Trent Black <97129943+Trentbyu@users.noreply.github.com>
2024-02-04 09:46:37 -0700

    updates

--- embedded/SERVER/FRONT-END/src/Esp32ConfigPage.js
+++ embedded/SERVER/FRONT-END/src/Esp32ConfigPage.js
@@ -1,39 +1,35 @@
 import React from 'react';
 import Esp32Config from './compoents/esp32Config';
 import Esp8266Config from './compoents/esp8266';
-import { motion } from 'framer-motion';
 import pageData from './HomePage.json'; // Import JSON directly
 
 const Esp32ConfigPage = () => {
-
-  // Keep track of seen IP addresses for each device type
-  const seenIPs = {
-    ESP32: new Set(),
-    ESP8266: new Set()
-  };
+  // Group components by IP address
+  const componentsByIP = {};
+  pageData.components.forEach(component => {
+    const ipAddress = component.props.imageSourceLink;
+    if (!componentsByIP[ipAddress]) {
+      componentsByIP[ipAddress] = component;
+    }
+  });
 
   return (
     <div className="min-h-screen flex flex-col py-10 bg-gray-500">
-      {pageData.components.map((component, index) => {
-        const { Device, ESPNAME, imageSourceLink } = component.props;
-        // If the IP address has already been seen for this device type, skip rendering
-        if (seenIPs[Device].has(imageSourceLink)) {
-          return null;
-        }
-        // If not, mark the IP address as seen for this device type and render the component
-        seenIPs[Device].add(imageSourceLink);
+      {Object.keys(componentsByIP).map((ipAddress, index) => {
+        const component = componentsByIP[ipAddress];
+        const { Device, ESPNAME } = component.props;
         return (
           <div key={index} className="flex flex-col sm:flex-row my-5 sm:mb-2">
             <div className="h-300 mx-auto p-4 mb-4 sm:mb-0">
               {Device === 'ESP32' && (
                 <Esp32Config
-                  apiEndpoint={imageSourceLink}
+                  apiEndpoint={ipAddress}
                   ESPNAME={ESPNAME}
                 />
               )}
               {Device === 'ESP8266' && (
                 <Esp8266Config
-                  temperatureApiEndpoint={imageSourceLink}
+                  temperatureApiEndpoint={ipAddress}
                   ESPNAME={ESPNAME}
                 />
               )}
