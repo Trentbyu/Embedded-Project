96617704990ff103b1f4dfa247dd3cfc215b5571
Trent Black <97129943+Trentbyu@users.noreply.github.com>
2024-02-03 19:22:22 -0700

    Update home.js

--- embedded/SERVER/FRONT-END/src/home.js
+++ embedded/SERVER/FRONT-END/src/home.js
@@ -12,7 +12,7 @@ const HomePage = () => {
         <div className="h-300 mx-auto p-4 mb-4 sm:mb-0">
           {/* ImageViewer component */}
          
-          {/* <ImageViewer imageSourceLink={"http://192.168.0.116"} containerId="192.168.0.116" ESPNAME="Inside" /> */}
+          <ImageViewer imageSourceLink={"http://192.168.0.116"} containerId="192.168.0.116" ESPNAME="Inside" />
         </div>
         <div className="h-300 mx-auto p-4 mb-4 sm:mb-0">
           {/* TemperatureViewer component */}
