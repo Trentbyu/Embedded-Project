fae3fa640125cdad93a32a66ba3e5a05052a6986
Trent Black <97129943+Trentbyu@users.noreply.github.com>
2024-02-09 17:47:11 -0700

    updating

--- embedded/arduinocode/AiCamRedo/backend.ino
+++ embedded/arduinocode/AiCamRedo/backend.ino
@@ -65,7 +65,7 @@ String sendPhoto() {
     readServerIPFromEEPROM(serverIPCharArray);
 
     // Convert char array to String
-    String serverIP = "192.168.1.156";
+    String serverIP = serverIPCharArray;
 
     // Construct server name using the retrieved server IP address
     
