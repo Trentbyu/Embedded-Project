bb6b0bc92bccbbd4d139c5c08fd19774c59f1272
Trent Black <97129943+Trentbyu@users.noreply.github.com>
2024-02-10 20:05:56 -0700

    Update getplaybaack.js

--- embedded/FRONT-END/src/compoents/getplaybaack.js
+++ embedded/FRONT-END/src/compoents/getplaybaack.js
@@ -1,30 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import ipAddress from '../index';
 
-function GetPlayback({  argument,espIp }) {
+function GetPlayback({ argument, espIp }) {
     const [gif, setGif] = useState('');
-  
+
     useEffect(() => {
-      fetch(`http://${ipAddress}:5000/gif?argument=${argument}?ipAddress=${espIp}`)
-        .then(response => {
-          // Assuming the response is a blob
-          return response.blob();
-        })
-        .then(blob => {
-          // Convert the blob into a URL
-          const gifUrl = URL.createObjectURL(blob);
-          setGif(gifUrl);
-        })
-        .catch(error => {
-          console.error('Error fetching GIF:', error);
-        });
-    }, [ipAddress, argument]);
-  
+        fetch(`http://${ipAddress}:5000/gif?argument=${argument}&ip_address=${espIp}`)
+            .then(response => {
+                // Assuming the response is a blob
+                return response.blob();
+            })
+            .then(blob => {
+                // Convert the blob into a URL
+                const gifUrl = URL.createObjectURL(blob);
+                setGif(gifUrl);
+            })
+            .catch(error => {
+                console.error('Error fetching GIF:', error);
+            });
+    }, [argument, espIp]);
+
     return (
-      <div>
-        {gif && <img src={gif} alt="GIF" />}
-      </div>
+        <div>
+            {gif && <img src={gif} alt="GIF" />}
+        </div>
     );
-  }
-  
-  export default GetPlayback;
+}
+
+export default GetPlayback;
