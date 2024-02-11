6cfba03e6e1a326cd6c3fa7c94bfbf93683eaad9
Trent Black <97129943+Trentbyu@users.noreply.github.com>
2024-02-10 19:46:51 -0700

    Update playbackTHERE.js

--- embedded/FRONT-END/src/compoents/playbackTHERE.js
+++ embedded/FRONT-END/src/compoents/playbackTHERE.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import ipAddress from '../index';
 import GetPlayback from './getplaybaack';
 
-function PlaybackFiles({  argument }) {
+function PlaybackFiles({ argument }) {
   const [files, setFiles] = useState([]);
   const [showPlayback, setShowPlayback] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [showFiles, setShowFiles] = useState(true); // New state for showing playback files
   const [showGif, setShowGif] = useState(true); 
+
   useEffect(() => {
     fetchFiles();
   }, []);
@@ -24,27 +26,30 @@ function PlaybackFiles({  argument }) {
 
   const handleClick = (fileName) => {
     setSelectedFile(fileName);
+    setShowPlayback(true); // Show playback when a file is clicked
     console.log(fileName)
   };
 
   return (
     <div className="p-4">
       <h2 className="text-xl font-bold mb-2">Playback Files:</h2>
-      <ul className="list-disc pl-4">
-        {files.map((file, index) => (
-          <li key={index} className="cursor-pointer text-blue-500 hover:underline" onClick={() => handleClick(file)}>
-            {file}
-          </li>
-        ))}
-      </ul>
+      <button className="mb-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" onClick={() => setShowFiles(!showFiles)}>
+        {showFiles ? 'Hide Files' : 'Show Files'}
+      </button>
+      {showFiles && (
+        <ul className="list-disc pl-4">
+          {files.map((file, index) => (
+            <li key={index} className="cursor-pointer text-blue-500 hover:underline" onClick={() => handleClick(file)}>
+              {file}
+            </li>
+          ))}
+        </ul>
+      )}
       {showPlayback && selectedFile && (
         <div>
           <div className="p-4 border border-gray-300 rounded-md shadow-md">
-            {showGif && <GetPlayback argument={selectedFile} />}
-            <button className="mt-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600" onClick={() => setShowGif(!showGif)}>
-              {showGif ? 'Hide' : 'Show'} GIF
-            </button>
-            <button className="ml-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600" onClick={() => setShowPlayback(false)}>Hide Get Playback</button>
+            <GetPlayback argument={selectedFile} />
+            <button className="mt-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600" onClick={() => setShowPlayback(false)}>Hide Get Playback</button>
           </div>
         </div>
       )}
