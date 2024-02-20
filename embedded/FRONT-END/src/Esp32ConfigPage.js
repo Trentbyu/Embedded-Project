e26de4c079f97a9bbe469c6bd806ff98a64415a9
Trentbyu <bla18016@byui.edu>
2024-02-20 23:27:20 +0000

    auto update

--- embedded/FRONT-END/src/Esp32ConfigPage.js
+++ embedded/FRONT-END/src/Esp32ConfigPage.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Esp32Config from './compoents/esp32Config';
 import Esp8266Config from './compoents/esp8266';
-import pageData from './HomePage.json'; // Import JSON directly
+import ipAddress from './index';
 
 const Esp32ConfigPage = () => {
+  const [components, setComponents] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://${ipAddress}:5000/api/components`)
+      .then(response => response.json())
+      .then(data => setComponents(data))
+      .catch(error => console.error('Error fetching components:', error));
+  }, []);
+
   // Group components by IP address
   const componentsByIP = {};
-  pageData.components.forEach(component => {
+  components.forEach(component => {
     const ipAddress = component.props.imageSourceLink.trim(); // Trim any leading or trailing spaces
     if (!componentsByIP[ipAddress]) {
       componentsByIP[ipAddress] = component;
@@ -15,32 +24,31 @@ const Esp32ConfigPage = () => {
 
   return (
     <div className="min-h-screen flex flex-col py-10 bg-gray-500">
-  <div className="flex flex-wrap justify-center">
-    {Object.keys(componentsByIP).map((ipAddress, index) => {
-      const component = componentsByIP[ipAddress];
-      const { Device, ESPNAME } = component.props;
-      return (
-        <div key={index} className="w-full sm:w-1/3 px-2 mb-4">
-          <div className="h-300 mx-auto p-4">
-            {Device === 'ESP32' && (
-              <Esp32Config
-                apiEndpoint={ipAddress}
-                ESPNAME={ESPNAME}
-              />
-            )}
-            {Device === 'ESP8266' && (
-              <Esp8266Config
-                temperatureApiEndpoint={ipAddress}
-                ESPNAME={ESPNAME}
-              />
-            )}
-          </div>
-        </div>
-      );
-    })}
-  </div>
-</div>
-
+      <div className="flex flex-wrap justify-center">
+        {Object.keys(componentsByIP).map((ipAddress, index) => {
+          const component = componentsByIP[ipAddress];
+          const { Device, ESPNAME } = component.props;
+          return (
+            <div key={index} className="w-full sm:w-1/3 px-2 mb-4">
+              <div className="h-300 mx-auto p-4">
+                {Device === 'ESP32' && (
+                  <Esp32Config
+                    apiEndpoint={ipAddress}
+                    ESPNAME={ESPNAME}
+                  />
+                )}
+                {Device === 'ESP8266' && (
+                  <Esp8266Config
+                    temperatureApiEndpoint={ipAddress}
+                    ESPNAME={ESPNAME}
+                  />
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
--- embedded/FRONT-END/src/home.js
+++ embedded/FRONT-END/src/home.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ImageViewer from './compoents/camera';
 import TemperatureViewer from './compoents/temp';
-import pageData from './HomePage.json'; // Import JSON directly
 import AqiViewer from './compoents/aqi';
 import HumidityViewer from './compoents/humidity';
+import ipAddress from './index';
+
 const HomePage = () => {
+  const [components, setComponents] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://${ipAddress}:5000/api/components`)
+      .then(response => response.json())
+      .then(data => setComponents(data))
+      .catch(error => console.error('Error fetching components:', error));
+  }, []);
+
   // Group components by room
-  const groupedComponents = pageData.components.reduce((acc, component) => {
+  const groupedComponents = components.reduce((acc, component) => {
     const { room } = component.props;
     if (!acc[room]) {
       acc[room] = [];
@@ -14,7 +24,7 @@ const HomePage = () => {
     acc[room].push(component);
     return acc;
   }, {});
-  
+
   return (
     <div className="min-h-screen flex flex-col py-10 bg-gray-500">
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
@@ -22,8 +32,8 @@ const HomePage = () => {
           <div key={index} className="mx-4 mb-8">
             <h2 className="text-2xl text-left mb-3 mx-2">{room}</h2>
             <div>
-              {groupedComponents[room].map((component, index) => (
-                <div key={index} className="mx-auto p-4">
+              {groupedComponents[room].map((component, componentIndex) => (
+                <div key={componentIndex} className="mx-auto p-4">
                   {component.type === 'ImageViewer' && (
                     <ImageViewer
                       imageSourceLink={component.props.imageSourceLink}
@@ -36,22 +46,21 @@ const HomePage = () => {
                       ESPNAME={component.props.ESPNAME}
                     />
                   )}
-                  {component.type === 'aqiViewer' && (
+                  {component.type === 'AqiViewer' && (
                     <AqiViewer
                       aqiApiEndpoint={component.props.imageSourceLink}
                       ESPNAME={component.props.ESPNAME}
                     />
                   )}
-                  {component.type === 'humidityViewer' && (
+                  {component.type === 'HumidityViewer' && (
                     <HumidityViewer 
-                      humidityApiEndpoint ={component.props.imageSourceLink}
+                      humidityApiEndpoint={component.props.imageSourceLink}
                       ESPNAME={component.props.ESPNAME}
                     />
                   )}
                 </div>
               ))}
             </div>
-           
           </div>
         ))}
       </div>
